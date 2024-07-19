4c670c39ba5e95848a7083146b0c071e014c124d
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-19 16:59:23 +0200

    added home style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <main>
+            <div className="w-full h-misura-header-picture bg-cover bg-no-repeat border-2 border-black bg-header-picture-2">
+
+            </div>
+            <div className="w-full flex flex-col justify-center items-center">
+                
+            </div>
+        </main>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-picture': "url('./src/assets/prodotti/galaxy-s24.png')",
+        'header-picture-2': "url(https://media-assets.wired.it/photos/666869709d6bb6d1aef15523/16:9/w_1920%2Cc_limit/classifica%2520migliori%2520smartphone%2520top%2520giugno%25202024.jpg)"
+      },
+      height: {
+        'misura-header-picture': "46.88rem"
+      }
+    },
   },
   plugins: [],
 }
