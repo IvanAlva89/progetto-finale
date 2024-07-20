7cb27fd89723633c588aa308e328646ecdb2b920
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-07-20 13:00:23 +0200

    modifiche tailwind.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import '../assets/importCss/style.css'
+
 const Home = () => {
     return (
-        <main>
+        <>
             <div className="w-full h-misura-header-picture bg-cover bg-no-repeat border-2 border-black bg-header-picture-2">
+                <div className="w-full flex flex-col justify-center items-center">
+                    <button className="bg-violet-200 hover:bg-violet-300 font-lato rounded-md px-4 py-2">Shop Now</button>
+                </div>
 
             </div>
-            <div className="w-full flex flex-col justify-center items-center">
-                
-            </div>
-        </main>
+        </>
     )
 }
 
