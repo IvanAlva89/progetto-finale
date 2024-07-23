b11a67e1452290ea49adf985207e8895cfaa50ca
alexdant91 <alexdant91@gmail.com>
2024-07-23 09:27:08 +0200

    Changed login layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -67,21 +67,47 @@ const Login = () => {
                     </div>
                 )
             }
-            <select value={role} onChange={handleChangeRole}>
-                <option value="user">User</option>
-                <option value="seller">Seller</option>
-            </select>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <input type="email" name="email" value={form.email} onInput={handleInput} />
+            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Accedi al tuo account</h2>
                 </div>
-                <div>
-                    <input type="password" name="password" value={form.password} onInput={handleInput} />
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleLogin}>
+                        <div>
+                            <label for="role" className="block text-sm font-medium leading-6 text-gray-900">Accedi come</label>
+                            <select id="role" value={role} onChange={handleChangeRole} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required>
+                                <option value="">Accedi come...</option>
+                                <option value="user">User</option>
+                                <option value="seller">Seller</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Indirizzo Email</label>
+                            <div className="mt-2">
+                                <input id="email" name="email" value={form.email} onInput={handleInput} type="email" autocomplete="email" required className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            </div>
+                            <div className="mt-2">
+                                <input id="password" name="password" value={form.password} onInput={handleInput} type="password" autocomplete="current-password" required className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Accedi</button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500 flex gap-2 items-center">
+                        Non hai un account?
+                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Registrati</a>
+                    </p>
                 </div>
-                <div>
-                    <button type="submit">Login</button>
-                </div>
-            </form>
+            </div>
         </>
     )
 }
