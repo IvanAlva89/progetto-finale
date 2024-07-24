042c49d3ee15a1c5a04d8a50225a584855203034
Zaccaria <zac.fede96@gmail.com>
2024-07-24 16:50:05 +0200

    footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,28 +1,82 @@
-const Footer = () => {
-    return (
-        <>
-            <footer class="bg-blue-100 rounded-lg shadow m-4">
-                    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                        <span class="text-sm text-gray-500 sm:text-center">© 2024 <a href="#" class="hover:underline">MediaTech™</a>. All Rights Reserved.
-                        </span>
-                    <ul class="flex flex-wrap items-center text-sm font-medium text-gray-500">
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                    </div>
-                </footer>
-        </>
-    )
-}
+import React from "react";
+import {
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaTwitter,
+  FaTwitch,
+} from "react-icons/fa";
 
-export default Footer
+const sections = [
+  {
+    title: "Solutions",
+    items: ["Marketing", "Analytics", "Commerce", "Data", "Cloud"],
+  },
+  {
+    title: "Support",
+    items: ["Pricing", "Documentation", "Guides", "API Status"],
+  },
+  {
+    title: "Company",
+    items: ["About", "Blog", "Jobs", "Press", "Partners"],
+  },
+  {
+    title: "Legal",
+    items: ["Claims", "Privacy", "Terms", "Policies", "Conditions"],
+  },
+];
+
+const items = [
+  { name: "Facebook", icon: FaFacebook, link: "https://www.facebook.com/" },
+  { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/" },
+  { name: "Twitter", icon: FaTwitter, link: "https://twitter.com/" },
+  { name: "Twitch", icon: FaTwitch, link: "https://www.twitch.tv/" },
+  { name: "Github", icon: FaGithub, link: "https://github.com/" },
+];
+
+const Footer = () => {
+  return (
+    <div className="w-full mt-24 text-black-500 py-y px-2 bg-[#2196f3]">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
+        {sections.map((section, index) => (
+          <div key={index}>
+            <h6 className="font-bold uppercase pt-2">{section.title}</h6>
+            <ul>
+              {section.items.map((item, i) => (
+                <li key={i} className="py-1 text-black-500 hover:text-white">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className="font-bold uppercase">Subscribe to our newsletter</p>
+          <p className="py-4">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <form className="flex flex-col sm:flex-row">
+            <input
+              className="w-full p-2 mr-4 rounded-md mb-4"
+              type="email"
+              placeholder="Enter email.."
+            />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-black-500">
+        <p className="py-4">2022 Workflow, LLC. All rights reserved</p>
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+          {items.map((x, index) => {
+            return <x.icon key={index} className="hover:text-white" />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,14 +11,14 @@ const Home = () => {
 
     return (
         <>
-            <div className="flex flex-col justify-center items-center relative w-full h-misura-header-picture bg-cover bg-no-repeat bg-header-picture-2">
-                <div className="flex flex-col gap-8">
+            <div className="flex flex-col justify-center relative w-full h-misura-header-picture bg-cover bg-no-repeat bg-header-picture-2">
+                <div className="flex flex-col gap-8 absolute left-64 top-12">
                     <h1 className="px-4 text-6xl text-center text-white font-lato font-extrabold leading-tight">
                         Il futuro della tecnologia a<br />
                         portata di tutti
                     </h1>
                     <div className="flex flex-col justify-center items-center">
-                        <Link to="/search" className="bg-violet-200 hover:bg-violet-300 font-lato font-bold rounded-md px-7 py-3">
+                        <Link to="/search" className="bg-violet-200 hover:bg-violet-300 font-lato font-bold rounded-md px-7 py-3 ">
                             Vai alla Ricerca
                         </Link>
                     </div>
@@ -64,12 +64,18 @@ const Home = () => {
                             <p>24/7 customer support bot</p>
                         </div>
                     </div>
-                    <div className="flex gap-2">
+                    <div className="flex gap-2 flex-col">
+                        <div>
+                        <h3 className="">Categories: </h3>
+                        <br />
+                        </div>
+                        <div className="flex gap-2">
                         {
                             categories.current.map(c => (
                                 <Link key={c.id} to={`/search?c=${c.name}`}>{c.name}</Link>
                             ))
                         }
+                        </div>
                     </div>
                     <div className="gap-3 flex p-6 rounded justify-center">
                         {products.map((product) => {
